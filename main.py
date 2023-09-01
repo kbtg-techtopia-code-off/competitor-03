fc2cd8ae065d706714d9f3ca7467297a0249ac63
kbtgtechtopia03@proton.me <kbtgtechtopia03@kasikornbank.com>
2023-09-01 15:08:23 +0700

    C2

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-# implement rock paper scissors game
-
+# implement rock paper scissors lizard and spock game
 import random
 
 # function to get user input
 def get_user_input():
     while True:
+        print("Choose your option: ")
+        print("1. Rock")
+        print("2. Paper")
+        print("3. Scissors")
+        print("4. Lizard")
+        print("5. Spock")
         user_input = input("Please input your choice >>> ")
-        if user_input not in ['rock', 'paper', 'scissors']:
+        if user_input == '1':
+            return 'rock'
+        elif user_input == '2':
+            return 'paper'
+        elif user_input == '3':
+            return 'scissors'
+        elif user_input == '4':
+            return 'lizard'
+        elif user_input == '5':
+            return 'spock'
+        else :
             print("Invalid input, please try again")
-        else:
-            return user_input
         print("==============================\n")
         
 def get_computer_input():
 
     # random string rock paper scissors
-    computer_input = random.choice(['rock', 'paper', 'scissors'])
+    computer_input = random.choice(['rock', 'paper', 'scissors', 'lizard', 'spock'])
     return computer_input
 
 count = 0
@@ -30,36 +43,52 @@ while True:
     if user_input == computer_input:
         continue
 
-    # computer choose rock and player choose paper'
-    elif computer_input == 'rock' and user_input == 'paper':
-        player_score+=1
+    # All cases that user wins
+
+    # user choose rock and computer choose scissors or lizard
+    if user_input == 'rock' and computer_input in ['scissors', 'lizard']:
         user_win = True
+        player_score += 1
 
-    # computer choose rock and player choose scissors
-    elif computer_input == 'rock' and user_input == 'scissors':
-        computer_score+=1
-        computer_win = True
-
-    # computer choose paper and player choose rock
-    elif computer_input == 'paper' and user_input == 'rock':
-        computer_score+=1
-        computer_win = True
-
-    # computer choose scissors and player choose paper
-    elif computer_input == 'scissors' and user_input == 'paper':
-        computer_score+=1
-        computer_win = True
-
-    # computer choose paper and player choose scissors
-    elif computer_input == 'paper' and user_input == 'scissors':
-        player_score+=1
+    # user choose paper and computer choose rock or spock   
+    elif user_input == 'paper' and computer_input in ['rock', 'spock']:
         user_win = True
+        player_score += 1
 
-    # computer choose scissors and player choose rock
-    elif computer_input == 'scissors' and user_input == 'rock':
-        player_score+=1
+    # user choose scissors and computer choose paper or lizard
+    elif user_input == 'scissors' and computer_input in ['paper', 'lizard']:
         user_win = True
+        player_score += 1
 
+    # user choose lizard and computer choose paper or spock
+    elif user_input == 'lizard' and computer_input in ['paper', 'spock']:
+        user_win = True
+        player_score += 1
+
+    # user choose spock and computer choose rock or scissors 
+    elif user_input == 'spock' and computer_input in ['rock', 'scissors']:
+        user_win = True
+        player_score += 1
+
+    # All cases that computer wins
+    elif computer_input == 'rock' and user_input in ['scissors', 'lizard']:
+        computer_score += 1
+
+    # user choose paper and computer choose rock or spock   
+    elif computer_input == 'paper' and user_input in ['rock', 'spock']:
+        computer_score += 1
+
+    # user choose scissors and computer choose paper or lizard
+    elif computer_input == 'scissors' and user_input in ['paper', 'lizard']:
+        computer_score += 1
+    # user choose lizard and computer choose paper or spock
+    elif computer_input == 'lizard' and user_input in ['paper', 'spock']:
+        computer_score += 1
+    # user choose spock and computer choose rock or scissors 
+    elif computer_input == 'spock' and user_input in ['rock', 'scissors']:
+        computer_score += 1
+        
+    
     count+=1
     print("The computer choice: ", computer_input)
     print("The player choice: ", user_input)
